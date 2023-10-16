7cc60e47578ded5d8506fd1b8e43b31c719a251d
m-raheem <raheemarif710@gmail.com>
2023-10-16 09:32:46 +0000

    SP21-BCT-018

---
+++ myapp018.java
@@ -0,0 +1,14 @@
+public class myapp018 {
+    public static printInfo() {
+        // Your information
+        String name = "Muhammad Raheem";
+        String regNo = "SP21-BCT-018";
+        String semester = "BCT-6";
+
+        // Print the information
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Semester: " + semester);
+    }
+}
+
